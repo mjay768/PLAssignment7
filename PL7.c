6d321dab01e8227a5058ac4145612e796a34b05d
P Manoj Reddy <manoj_ponagandla@hotmail.com>
2019-05-07 00:04:56 -0500

    Update PL7.c

--- PL7.c
+++ PL7.c
@@ -4,42 +4,13 @@
 
 #define MAX_SIZE 1000
 
-struct Stack{
-
-    int top;
-    unsigned limit;
-    char *arr;
-};
-
-struct stack* createStack(unsigned limit){
-    struct Stack* stack = (struct Stack*)malloc(sizeof(sizeof(struct Stack)));
-
-    stack -> limit = limit;
-    stack -> top = -1;
-    stack -> arr = (char*) malloc(sizeof(char *));
-    return stack;
-}
- 
-int isFull(struct Stack* stack)
-{
-    return (stack ->top == stack -> limit -1);
-}
-
-int isEmpty(struct Stack* stack)
-{
-    return (stack -> top == -1);
-}
-
 int f1();
 int f2();
 int f3();
 
-struct Stack* stack;
-
 int main()
 {
-           
-
+    long int item;
     f1();
 
     return 0;
@@ -49,19 +20,35 @@ int main()
 int f1()
 {
     char ch[MAX_SIZE];
+    long int item;
     static int n;
     static long int addr;
     static int AR[MAX_SIZE];
-    addr = &AR[n] - addr;
+    addr = (long int)ch;
     
 
     if(n<10)
     {
 
-        printf("\nCall #%d at %ld",n+1,&ch[0]);
-        printf("\nAR Size       #%d is %ld",n+1,addr);
+        printf("\nCall #%d at %x",n+1,(void *)ch);
+        printf("\nAR Size       #%d is %ld",n+1,addr - (long int)ch);
         f1(n++);
     }
+        
+
+
+}
+
+int f2()
+{
+    char ch[MAX_SIZE];
+    static int n;
+    static long int addr;
+    static int AR[MAX_SIZE];
+    addr = &AR[n] - addr;
+    printf("\nCall #%d at %ld",n+1,&ch[0]);
+    printf("\nAR Size       #%d is %ld",n+1,addr);
+    f2(n++);
         
 
 
