bcb484701d30aa2f35958fca1c37a55b1ab845e1
P Manoj Reddy <manoj_ponagandla@hotmail.com>
2019-05-08 22:23:37 -0500

    Update PL7.c

--- PL7.c
+++ PL7.c
@@ -12,7 +12,7 @@ int main()
 {
     long int item;
     //f1();
-    //f2();
+    //f2();//
     f3();
 
     return 0;
@@ -51,8 +51,9 @@ int f2()
     static int AR[MAX_SIZE];
     long int size = addr - (long int)ch;
     addr = (long int)ch;
-    printf("\nCall #%d at %p",n+1,(void *)ch);
+    printf("\nCall #%d at 000000000%X",n+1,(void *)ch);
     printf("\nAR Size       #%d is %ld",n+1,size);
+    printf("\nStack size    #%d	is   %ld",n+1,size*(n+1));	
     f2(n++);
 
 }
